c8f262cb7ee070613813ad82872203273b5522e1
Nenoeglah <134705870+Nenoeglah@users.noreply.github.com>
2023-06-18 23:39:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName ="bob";
+function upperCaseCustomerName(bob) {
+   return customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+   setBestcustomer;
+}
+ function overwriteBestCustomer() {
+   return overwritescustomer ="maybe bob"
+}
+const changeLeastFavoriteCustomer = "some initial value";
+
+changeLeastFavoriteCustomer() 
+
+  
+
+
+
+
+
+
+
